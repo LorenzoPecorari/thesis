8f9b6a66363418bf560633dfb2947377b7527fd4
Lorenzo Pecorari <pecorari.1885161@studenti.uniroma1.it>
2026-01-30 20:19:15 +0100

    aggregated states run 4k 172 4 agents

--- scripts/multi-agent/custom-environment/mas_env/nn/aggregated_states/custom_environment.py
+++ scripts/multi-agent/custom-environment/mas_env/nn/aggregated_states/custom_environment.py
@@ -67,7 +67,7 @@ class CustomEnvironment(ParallelEnv):
         # internal counters for episode compeltion 
         self.timestep = 0
         self.max_steps = (3600 / proc_interval) * 5
-        self.episode = 355
+        self.episode = 172
         self._w = w
         
         try:
@@ -258,7 +258,7 @@ class CustomEnvironment(ParallelEnv):
 
         infos = {a: {} for a in self.agents}
         
-        self.episode = 355
+        self.episode = 172
 
         return observations, infos
         
@@ -378,7 +378,7 @@ class CustomEnvironment(ParallelEnv):
         truncations = {a: False for a in self.agents}
         
         if(self.timestep == (self.max_steps - 1)):
-            self.episode = 355
+            self.episode = 172
             truncations = {a: True for a in self.agents}
         
         self.timestep += 1
