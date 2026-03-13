48390b2396c82ee7c5d8e5cdb86c083511ecf25f
Lorenzo Pecorari <pecorari.1885161@studenti.uniroma1.it>
2026-03-13 13:55:41 +0100

    plotted results for once a time training

--- scripts/multi-agent/custom-environment/mas_env/nn/once_a_time/plotter.py
+++ scripts/multi-agent/custom-environment/mas_env/nn/once_a_time/plotter.py
@@ -152,8 +152,8 @@ def plot_backlogs(folder_path):
     
 # print(len(os.listdir(folder_path)))
 
-plot_rewards(folder_path)
+# plot_rewards(folder_path)
 # plot_framerate(folder_path)
 
 # plot_batteries(folder_path)
-# plot_backlogs(folder_path)
+plot_backlogs(folder_path)
